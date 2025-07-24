5b90bbd0784ce0d2f443094c64a8b329b1be5042
Sandhya Raj <94752313+sheeny1213@users.noreply.github.com>
2025-07-24 14:34:46 +0530

    Initial commit

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9I7r2W3iykOc-1YD_AgeFIofXG7cktKU",
-  authDomain: "super-mall-fc59e.firebaseapp.com",
-  projectId: "super-mall-fc59e",
-  storageBucket: "super-mall-fc59e.appspot.com",
-  messagingSenderId: "397561772027",
-  appId: "1:397561772027:web:bd41fbfc8832901c1efd5e"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
